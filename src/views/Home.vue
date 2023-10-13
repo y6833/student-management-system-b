64d0d305b132878b07f5c73348b874a55a66083c
SleepRain <1819503703@qq.com>
2023-10-13 22:01:47 +0800

    基本框架搭建完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,220 @@
 <template>
-  <div class="home">
-    <el-button type="primary">主要按1钮</el-button>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container style="height: 100vh">
+    <el-aside
+      :width="sideWidth + 'px'"
+      style="
+        background-color: rgb(238, 241, 246);
+        box-shadow: 2px 0 6px rgb(0 21 41 / 35%);
+      "
+    >
+      <el-menu
+        :default-openeds="['1', '3']"
+        style="min-height: 100%; overflow-x: hidden"
+        background-color="rgb(48, 65, 86)"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse-transition="false"
+        :collapse="isCollapse"
+        class="el-menu-vertical-demo"
+      >
+        <div style="height: 60px; line-height: 60px; text-align: center">
+          <img
+            src="@/assets/logo.png"
+            style="width: 20px; position: relative; top: 5px"
+          />
+          <b style="color: #fff; margin-left: 5px" v-show="logoTextShow"
+            >视教学习管理系统</b
+          >
+        </div>
+        <el-submenu index="1">
+          <template slot="title"
+            ><i class="el-icon-message"></i>
+            <span slot="title">导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"
+            ><i class="el-icon-menu"></i
+            ><span slot="title">导航二</span></template
+          >
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"
+            ><i class="el-icon-setting"></i>
+            <span slot="title">导航三</span></template
+          >
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header
+        style="
+          background-color: #fff;
+          font-size: 12px;
+          border-bottom: 1px solid #ccc;
+          line-height: 60px;
+          display: flex;
+        "
+      >
+        <div style="flex: 1; font-size: 20px">
+          <span
+            :class="collapseBtnClass"
+            style="cursor: pointer"
+            @click="collapse"
+          ></span>
+        </div>
+        <el-dropdown style="width: 70px; cursor: pointer">
+          <span>王小虎</span
+          ><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+
+      <el-main>
+        <!-- 页签 -->
+        <div style="margin-bottom: 30px">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+
+        <!-- 搜索 -->
+        <div style="margin: 10px 0">
+          <el-input style="width: 200px" placeholder="请输入内容" v-model="input" suffix-icon="el-icon-search" />
+          <el-button style="margin-left: 5px" type="primary">搜索</el-button>
+        </div>
+
+        <!-- 新增 -->
+        <div style="margin: 10px 0">
+          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+          <el-button type="primary">导入 <i class="el-icon-upload"></i></el-button>
+          <el-button type="primary">导出 <i class="el-icon-download"></i></el-button>
+
+        </div>
+
+
+        <el-table :data="tableData" border stripe>
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+          <el-table-column label="操作" width="200">
+            <template slot-scope>
+              <el-button type="primary" size="mini">编辑</el-button>
+              <el-button type="danger" size="mini">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+
+        <!-- 分页 -->
+        <div style="padding: 10px 0">
+          <el-pagination
+            :page-sizes="[5, 10, 15, 20]"
+            :page-size="10"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400"
+          >
+          </el-pagination>
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
 }
+
+.el-aside {
+  color: #333;
+}
+</style>
+<script>
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(10).fill(item),
+      collapseBtnClass: "el-icon-s-fold",
+      isCollapse: false,
+      sideWidth: 200,
+      logoTextShow: true,
+    };
+  },
+  methods: {
+    /**
+    + * 切换组件的收缩状态。
+    + */
+    collapse() {
+      //点击收缩
+      // 切换收缩状态
+      this.isCollapse = !this.isCollapse;
+
+      if (this.isCollapse) {
+        // 如果收缩，将侧边栏宽度设置为 64
+        this.sideWidth = 64;
+        // 更新收缩按钮的样式类
+        this.collapseBtnClass = "el-icon-s-unfold";
+        this.logoTextShow = false;
+      } else {
+        // 如果未收缩，将侧边栏宽度设置为 200
+        this.sideWidth = 200;
+        // 更新收缩按钮的样式类
+        this.collapseBtnClass = "el-icon-s-fold";
+        this.sidewidth = 200;
+        this.logoTextShow = true;
+      }
+    },
+  },
+};
 </script>
