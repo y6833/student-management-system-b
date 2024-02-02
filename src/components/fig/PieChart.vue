534928550b3adb5979ef229ca43bcd46d437e611
SleepRain <1819503703@qq.com>
2024-02-03 00:16:22 +0800

    完善图表

---
+++ src/components/fig/PieChart.vue
@@ -0,0 +1,17 @@
+<!-- 饼图 -->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import * as echarts from "echarts"
+export default {
+    name: 'PieChart',
+}
+</script>
+
+<style scoped>
+
+</style>
